894182d71cdb14e31ba015dfdbb720e766f755dc
jeon-chanhee <chjeon95@naver.com>
2022-04-13 09:29:19 +0900

    Feat: upbit market data 조회 기능 추가

--- upbit.py
+++ upbit.py
@@ -13,3 +13,12 @@ def candle_refer(type, time , coin, count):
 #print(candle_refer('minutes','60','KRW-BTC','10'))
 
 def market_refter():
+
+    url = "https://api.upbit.com/v1/market/all?isDetails=false"
+
+    headers = {"Accept": "application/json"}
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.json()
+
